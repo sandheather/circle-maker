a20a95863a964fffa3e25aa85f733de5f0f7a291
sandheather <heather.e.sanders22@gmail.com>
2016-03-18 16:48:15 -0400

    added onto functions

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
 var $body = $('body');
 
-$('html').on('keydown', function () {
-  var $ball = $('<div>');
-  var newLeft = Math.random() * 1000;
-  var newTop = Math.random() * 1000;
+var makeCircle = function () {
+  var $newCircle = $('<div>');
+  var newRed = Math.floor(Math.random() * 256);
+  var newGreen = Math.floor(Math.random() * 256);
+  var newBlue = Math.floor(Math.random() * 256);
 
-  $body.append($ball);
-  $ball.css('left', newLeft);
-  $ball.addClass('ball');
-  $ball.css('top', newTop);
+  $newCircle.css('top', Math.random() * document.documentElement.clientHeight - 22);
+  $newCircle.css('left', Math.random() * document.documentElement.clientWidth - 22);
+  $newCircle.css('background-color', 'rgb(' + newRed + ', ' + newGreen + ', ' + newBlue + ')');
+  $newCircle.addClass('ball');
+
+  $body.append($newCircle);
+};
+
+$('html').on('keydown', function (e) {
+
+  var numCircles = Math.floor(Math.random() * 2);
+  var i = 0;
+
+  for (i = 0; i < numCircles; i++) {
+    makeCircle();
+  }
+
 });
